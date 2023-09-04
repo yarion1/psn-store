beb798e7b0866d1f9e9a84c5dce02fcd6f0bb9b3
yarion1 <pablo.dmg@outlook.com>
2023-09-04 00:38:25 -0300

    base psn store

---
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pricing',
+  templateUrl: './card-pricing.component.html',
+  styleUrls: ['./card-pricing.component.css']
+})
+export class CardPricingComponent {
+  @Input()
+  gameType: string = 'Digital Ps4';
+  @Input()
+  gamePrice: string = '$59.99';
+  constructor() {}
+}
